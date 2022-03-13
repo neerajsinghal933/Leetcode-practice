3312297584c0300952f89728670812c1ba26d68f
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-03-13 18:32:28 +0530

    Time: 93 ms (21.86%), Space: 45.2 MB (18.82%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,40 +1,56 @@
 class Solution {
 public:
-    map<vector<int>, int>m;
+    // map<vector<int>, int>m;
+    set<vector<int>>st;
     vector<vector<int>> combinationSum(vector<int>& coin, int target) {
         vector<vector<int>>res;
         vector<int>temp;
         int n = coin.size();
-        fun(coin, target, temp, res);
+        fun(coin, target, temp, res, n);
         
         return res;
     }
     
-    void fun(vector<int>&coin, int t, vector<int>temp, vector<vector<int>>&res)
+    void fun(vector<int>&coin, int t, vector<int> temp, vector<vector<int>>&res, int n)
     {
-        if(t==0)
+        if(t == 0 || n == 0)
         {
-            sort(temp.begin(), temp.end());
-            if(!m[temp])
+            if(t==0)
             {
-                m[temp] = 1;
                 res.push_back(temp);
-                               
             }
-            return; 
+            return;
         }
-        for(int i=0;i<coin.size();i++)
+        fun(coin, t, temp, res, n-1);
+        if(coin[n-1] <= t)
         {
-            if(t>=coin[i])
-            {
-                temp.push_back(coin[i]);
-                fun(coin, t-coin[i], temp, res);
-                temp.pop_back();
-            }
-            // else
-            // {
-            //     fun()
-            // }
+            temp.push_back(coin[n-1]);
+            fun(coin, t-coin[n-1], temp, res, n);
         }
+//         if(t==0)
+//         {
+//             sort(temp.begin(), temp.end());
+//             if(!st[temp])
+//             {
+//                 // m[temp] = 1;
+//                 st.insert(temp);
+//                 res.push_back(temp);
+                               
+//             }
+//             return; 
+//         }
+//         for(int i=0;i<coin.size();i++)
+//         {
+//             if(t>=coin[i])
+//             {
+//                 temp.push_back(coin[i]);
+//                 fun(coin, t-coin[i], temp, res);
+//                 temp.pop_back();
+//             }
+//             // else
+//             // {
+//             //     fun()
+//             // }
+//         }
     }
 };
