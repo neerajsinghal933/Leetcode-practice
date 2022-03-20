888f0f57d6fea49e319e3c60bf7b64f95a31c73f
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-03-20 09:35:56 +0530

    Time: 235 ms (25.18%), Space: 111.6 MB (29.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
         int n = tops.size();
-        int ans = INT_MAX;
-        for(int i=1;i<=6;i++)
+        
+        vector<int> faceA(7), faceB(7), same(7);
+        
+        for(int i = 0; i < n; ++i)
         {
-            int cnt = 0;
-            for(int j=0;j<n;j++)
-            {
-                if(tops[j]==i or bottoms[j]==i)
-                    cnt++;
-            }
-            if(cnt==n)
-            {
-                int temp = max(count(tops.begin(), tops.end(), i), count(bottoms.begin(), bottoms.end(), i));
-                ans = min(ans, n-temp);
-                    
-            }
+            ++faceA[tops[i]];
+            ++faceB[bottoms[i]];
+            
+            if(tops[i] == bottoms[i])
+                ++same[tops[i]];
         }
-        return ans==INT_MAX?-1:ans;
+        
+        int minRotation = INT_MAX;
+        
+        for(int i = 1; i<=6; ++i)
+        {
+            if(faceA[i] + faceB[i] - same[i] == n)
+                minRotation = min(minRotation , min(faceA[i],faceB[i]) - same[i]);
+        }
+        
+        return minRotation == INT_MAX ? -1 : minRotation;
+        
     }
 };
