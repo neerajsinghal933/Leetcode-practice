cabbf98b9d697cedac5a15264ea73f63a79b5d25
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-04-16 08:38:36 +0530

    Time: 400 ms (38.46%), Space: 7.6 MB (21.63%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int ans =  INT_MIN;
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]!=0)
+                    fun(grid, n, m, i, j, vis, grid[i][j]);
+            }
+        }
+        return ans==INT_MIN?0:ans;
+    }
+    
+    void fun(vector<vector<int>>&grid, int n, int m, int i, int j, vector<vector<int>>&vis, int cnt)
+    {
+        
+        if(i<0 or i>=n or j<0 or j>=m or !grid[i][j] )
+        {
+            ans = max(ans, cnt);
+            return;
+        }
+        // if(vis[i][j])
+        //     return;
+        vis[i][j] = 1;
+        int k = grid[i][j];
+        grid[i][j] = 0;
+        if(i+1<n)
+            fun(grid, n, m, i+1, j, vis, cnt + grid[i+1][j]);
+        if(i-1>=0)
+            fun(grid, n, m, i-1, j, vis, cnt + grid[i-1][j]);
+        if(j+1<m)
+            fun(grid, n, m, i, j+1, vis, cnt + grid[i][j+1]);
+        if(j-1>=0)
+            fun(grid, n, m, i, j-1, vis, cnt + grid[i][j-1]);
+        vis[i][j] = 0;
+        grid[i][j] = k;
+    }
+};
