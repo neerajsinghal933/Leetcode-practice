406b43c936875383396dd1baf0f49a004719e231
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-05-24 19:41:15 +0530

    Time: 153 ms (35.19%), Space: 184.3 MB (5.16%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,20 +1,26 @@
 class MyHashSet {
 public:
-    map<int, int>m;
+    // map<int, int>m;
+    int hash[1000001];
     MyHashSet() {
-        
+        memset(hash, 0, sizeof(hash));
     }
     
     void add(int key) {
-        m[key] = 1;
+        // m[key] = 1;
+        hash[key]=1;
     }
     
     void remove(int key) {
-        m[key] = 0;
+        // m[key] = 0;
+        hash[key] = 0;
     }
     
     bool contains(int key) {
-        if(m[key]==0)
+        // if(m[key]==0)
+        //     return false;
+        // return true;
+        if(hash[key]==0)
             return false;
         return true;
     }
