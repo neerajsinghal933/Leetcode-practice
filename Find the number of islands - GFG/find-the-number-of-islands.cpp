1e5f8b5e5948208731929ef025aad50e3bd32177
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-04 23:36:30 +0530

    Added solution - LeetHub

---
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -0,0 +1,70 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the number of islands.
+    
+    bool isSafe(int x, int y, vector<vector<int>>&vis, vector<vector<char>>&grid)
+    {
+        if(x>=0 and y>=0 and x<grid.size() and y<grid[0].size() and !vis[x][y] and grid[x][y]=='1')
+            return true;
+            return false;
+    }
+    
+    void dfs(int x, int y, vector<vector<int>>&vis, vector<vector<char>>&grid)
+    {
+        vis[x][y] = true;
+        vector<int>dirX = {1, 1, 1, 0, 0, -1, -1, -1};
+        vector<int>dirY = {1, 0, -1, 1, -1, 1, 0, -1};
+        for(int i=0;i<8;i++)
+        {
+            if(isSafe(x+dirX[i], y+dirY[i], vis, grid)==true)
+                dfs(x+dirX[i], y+dirY[i], vis, grid);
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1' and vis[i][j]==0)
+                {
+                    // cout<<"k";
+                    cnt++;
+                    dfs(i, j, vis, grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> grid(n, vector<char>(m, '#'));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        int ans = obj.numIslands(grid);
+        cout << ans << '\n';
+    }
+    return 0;
+}  // } Driver Code Ends
