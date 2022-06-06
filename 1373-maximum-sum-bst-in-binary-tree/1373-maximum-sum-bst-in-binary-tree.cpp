f305b7db60aa6fce14d4425d6b77241f4763f058
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-06 16:33:25 +0530

    Time: 294 ms (36.74%), Space: 92.4 MB (99.53%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    int maxSumBST(TreeNode* root) {
+        if(!root)
+        {
+            return 0;
+        }
+        solve(root);
+        return ans;
+    }
+    
+    void solve(TreeNode* root)
+    {
+        if(!root)
+            return;
+        int sum = 0;
+        if(check(root, INT_MIN, INT_MAX)==true)
+        {
+            get_sum(root, sum);
+            ans = max(ans, sum);
+            return;
+        }
+        solve(root->left);
+        solve(root->right);
+    }
+    
+    bool check(TreeNode* root, int a, int b)
+    {
+        if(!root)
+            return 1;
+        if(root->val <= a or root->val >= b)
+            return 0;
+        return check(root->left, a, root->val) and  check(root->right, root->val, b);
+    }
+    
+    int get_sum(TreeNode* root, int &sum)
+    {
+        if(!root)
+            return 0;
+        int x = get_sum(root->left, sum) + get_sum(root->right, sum) + root->val;
+        sum = max(sum, x);
+        return x;
+    }
+    
+};
