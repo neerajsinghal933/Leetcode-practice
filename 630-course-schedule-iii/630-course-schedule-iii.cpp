0dfec8d6851edd124101a6de439809de3a9598ec
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-07 17:04:21 +0530

    Time: 1450 ms (10.54%), Space: 272.5 MB (6.09%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    static bool cmp(const vector<int>&a, const vector<int>&b)
+    static bool comp(vector<int> a, vector<int> b)
     {
-        return a[1]<b[1];
+        return a[1] < b[1];
     }
-    
     int scheduleCourse(vector<vector<int>>& c) {
-        sort(c.begin(), c.end(), cmp);
-        priority_queue<int>pq;
-        
+        sort(c.begin(), c.end(), comp);
+        priority_queue<int> pq;
         int day = 0;
-        for(int i=0;i<c.size();i++)
+        // int cnt = 0;
+        for(auto x : c)
         {
-            if(c[i][1] >= day + c[i][0])
+            // cout<<x[0]<<"  "<<x[1]<<endl;
+            if((day + x[0]) <= x[1])
             {
-                day = day + c[i][0];
-                pq.push(c[i][0]);
+                // cnt++;
+                day += x[0];
+                pq.push(x[0]);
+                // cout<<"  "<<day<<endl;
             }
-            
-            else if(!pq.empty() and pq.top() > c[i][0])
+            else
             {
-                int temp = pq.top();
-                pq.pop();
-                day -= temp;
-                day += c[i][0];
-                pq.push(c[i][0]);
+                if(!pq.empty() && pq.top() > x[0])
+                {
+                    day = (day -pq.top() +x[0]);
+                    pq.pop();
+                    pq.push(x[0]);
+                    // cout<<" taken  "<<day<<endl;
+                }
             }
         }
         return pq.size();
