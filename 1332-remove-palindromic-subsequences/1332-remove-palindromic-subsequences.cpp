fb01896ead0f8223029dda75174c508ad9f2e544
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-08 09:23:03 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -3,7 +3,7 @@ public:
     bool isPlain(string s)
     {
         int n = s.size();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<=n/2;i++)
         {
             if(s[i]!=s[n-i-1])
                 return false;
