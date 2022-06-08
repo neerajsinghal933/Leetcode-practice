3ce48f624baa72324378ca94fa3564f21006bac9
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-08 19:49:10 +0530

    Added solution - LeetHub

--- M-Coloring Problem - GFG/mcoloring-problem.cpp
+++ M-Coloring Problem - GFG/mcoloring-problem.cpp
@@ -4,30 +4,29 @@ using namespace std;
 
 
  // } Driver Code Ends
-//Function to determine if graph can be coloured with at most M colours such
-//that no two adjacent vertices of graph are coloured with same colour.
+// Function to determine if graph can be coloured with at most M colours such
+// that no two adjacent vertices of graph are coloured with same colour.
 
-bool isSafe(bool graph[101][101], int v, int x, vector<int>&col, int idx)
+bool isSafe(bool graph[101][101], int n, int x, int idx, vector<int>&col)
 {
-    for(int i=0;i<v;i++)
+    for(int i=0;i<n;i++)
     {
         if(graph[idx][i]==1 and col[i]==x)
-        return false;
+            return false;
     }
     return true;
 }
 
-bool fun(bool graph[101][101], int m, int v, vector<int>&col, int idx)
+bool fun(bool graph[101][101], int m, int n, vector<int>&col, int idx)
 {
-    if(idx==v)
-    return true;
-    
+    if(idx==n)
+        return true;
     for(int i=1;i<=m;i++)
     {
-        if(isSafe(graph, v, i, col, idx)==true)
+        if(isSafe(graph, n, i, idx, col)==true)
         {
             col[idx] = i;
-            if(fun(graph, m, v, col, idx+1))
+            if(fun(graph, m, n, col, idx+1)==true)
                 return true;
             col[idx] = 0;
         }
@@ -35,14 +34,15 @@ bool fun(bool graph[101][101], int m, int v, vector<int>&col, int idx)
     return false;
 }
 
-bool graphColoring(bool graph[101][101], int m, int V)
-{
+
+bool graphColoring(bool graph[101][101], int m, int n) {
     // your code here
-    vector<int>col(V, 0);
-    if(fun(graph, m, V, col, 0)==true)
-    return true;
+    vector<int>col(n, 0);
+    if(fun(graph, m, n, col, 0)==true)
+        return true;
     return false;
 }
+
 
 // { Driver Code Starts.
 
