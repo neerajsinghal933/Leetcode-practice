22356c94f985d27d691df9e0032919bc38909028
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-09 11:26:02 +0530

    Added solution - LeetHub

--- Maximum product subset of an array - GFG/maximum-product-subset-of-an-array.cpp
+++ Maximum product subset of an array - GFG/maximum-product-subset-of-an-array.cpp
@@ -8,36 +8,34 @@ using namespace std;
 //User function Template for C++
 class Solution{
     public:
-    long long int findMaxProduct(vector<int>&a, int n){
-        //Write your code here 
-        int mod = 1e9+7;
-        if(n==1)
-            return a[0];
-        int max_neg = INT_MIN;
-        int neg = 0, zero = 0;
-        long long ans = 1;
-        for(int i=0;i<n;i++)
-        {
-            if(a[i]==0)
-            {
-                zero++;continue;
-            }
-            if(a[i]<0)
-            {
-                neg++;
-                max_neg = max(max_neg, a[i]);
-            }
-            ans = (ans*a[i])%mod;
-        }
-        if(zero==n)
-            return 0;
-        if(neg & 1)
-        {
-            if(neg == 1 and zero > 0 and zero + neg == n)
-                return 0;
-            ans = ans / max_neg;
-        }
-        return ans;
+    long long int findMaxProduct(vector<int>&arr, int n){
+        //Write your code here  
+         long long ans = 1;
+         long long mod = 1e9+7;
+	    int neg = 0, zero = 0, max_neg = INT_MIN;
+	    if(n==1)
+	        return arr[0];
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]==0)
+	        {
+	            zero++;
+	            continue;
+	        }
+	        if(arr[i]<0)
+	        {
+	            neg++;
+	            max_neg = max(max_neg, arr[i]);
+	        }
+	        ans = (ans*arr[i])%mod;
+	    }
+	    if(zero==n)
+	        return 0;
+	    if(ans < 0 and neg>0 and zero>0 and neg + zero==n)
+	        return 0;
+	   if(ans<0)
+	    ans /= max_neg;
+	    return ans;
     }
 };
 
