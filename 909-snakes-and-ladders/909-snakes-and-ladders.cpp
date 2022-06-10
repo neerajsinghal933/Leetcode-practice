390a9fa4414a3ad58429c89b9630ae9cbcb4d746
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-10 11:32:35 +0530

    Time: 21 ms (77.49%), Space: 12.6 MB (79.70%) - LeetHub

---
+++ 909-snakes-and-ladders/909-snakes-and-ladders.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int n = board.size();
+        vector<int>vis(401, 0);
+        queue<int>q;
+        q.push(1);
+        vis[1] = 1;
+        int cnt = 0;
+        while(!q.empty())
+        {
+            cnt++;
+            int sz = q.size();
+            for(int i=0;i<sz;i++)
+            {
+                int t = q.front();
+                q.pop();
+                for(int k=1;k<=6;k++)
+                {
+                    // cout<<k<<" ";
+                    int d = t + k;
+                    if(d>n*n)
+                    {
+                        // cout<<"m";
+                        break;
+                    }
+                    // cout <<d<<" ";
+                    if(vis[d]==1)
+                        continue;
+                    vis[d] = 1;
+                    d = fun(board, n, d);
+                    // cout << d;
+                    if(d==n*n)
+                        return cnt;
+                    q.push(d);
+                    // cout<<"p";
+                }
+            }
+        }
+        return -1;
+    }
+    
+    int fun(vector<vector<int>>&board, int n, int d)
+    {
+        int r, c, k;
+        r  = (n-1) - (d-1)/n;
+        c = (d-1)%n;
+        if(r%2==n%2)
+            c = n-1-c;
+        if(board[r][c]==-1)
+            return d;
+        return board[r][c];
+    }
+};
