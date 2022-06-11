bdb87124b967184d3731b6330f2dc456a50b6c02
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-11 23:52:22 +0530

    Time: 33 ms (48.36%), Space: 10.8 MB (64.17%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,11 +3,11 @@ public:
      int t[1001][1001];
     int lcs(string &a, string &b,int n,int m)
     {
-        // if(n==0 || m==0)
-        //     return t[n][m] = 0;
-        if(n==0 or m==0)
-            return 0;
-        if(t[n][m]!=0)
+        if(n==0 || m==0)
+            return t[n][m] = 0;
+        // if(n==0 or m==0)
+        //     return 0;
+        if(t[n][m]!=-1)
             return t[n][m];
         if(a[n-1]==b[m-1])
             return t[n][m]=1+lcs(a,b,n-1,m-1);
@@ -17,7 +17,7 @@ public:
     int longestCommonSubsequence(string text1, string text2) 
     {
         int n=text1.size(),m=text2.size();
-        memset(t,0,sizeof(t));
+        memset(t,-1,sizeof(t));
         return lcs(text1,text2,n,m);   
     }
 };
