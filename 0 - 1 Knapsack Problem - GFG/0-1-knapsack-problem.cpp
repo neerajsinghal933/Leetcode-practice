1557547a7cc4613020a52a1577b69781122753a6
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-11 10:35:13 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,40 +8,28 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int t[1001][1001];
-    
-    
-    int fun(int W, int wt[], int val[], int n)
-    {
-        int x = W;
-    //     int t[n+1][W+1];
-    //   memset(t, -1, sizeof(t));
-       if(n==0 || W==0)
-       return 0;
-       if(t[n][W]!=-1)
-       return t[n][W];
-       else if(wt[n-1]<=W)
-       return t[n][W] = max(val[n-1]+fun(W-wt[n-1], wt, val, n-1), fun(W, wt, val, n-1));
-       else
-       return t[n][W] = fun(W, wt, val, n-1);
-    }
-    
-    int knapSack(int W, int wt[], int val[], int n) 
+    int knapSack(int w, int wt[], int val[], int n) 
     { 
        // Your code here
-    //     int x = W;
-    // //     int t[n+1][W+1];
-    // //   memset(t, -1, sizeof(t));
-    //   if(n==0 || W==0)
-    //   return 0;
-    //   if(t[n][W]!=-1)
-    //   return t[n][W];
-    //   else if(wt[n-1]<=W)
-    //   return t[n][W] = max(val[n-1]+knapSack(W-wt[n-1], wt, val, n-1), knapSack(W, wt, val, n-1));
-    //   else
-    //   return t[n][W] = knapSack(W, wt, val, n-1);
-       memset(t, -1, sizeof(t));
-       return fun(W, wt, val, n);
+       int dp[n+1][w+1];
+       memset(dp, -1, sizeof(dp));
+       for(int i=0;i<=n;i++)
+        dp[i][0]=0;
+        for(int i=0;i<=w;i++)
+        dp[0][i] = 0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=w;j++)
+            {
+                if(j-wt[i-1]>=0)
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i-1][j-wt[i-1]] + val[i-1]);
+                }
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][w];
     }
 };
 
