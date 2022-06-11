27d4e394be15de3189549536a865fbba4228b8e5
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-11 09:59:13 +0530

    Time: 1139 ms (5.09%), Space: 212.3 MB (5.08%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -4,34 +4,33 @@ public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
         ll sum = 0;
-        for(int i:nums)
-            sum += i;
-        if(sum<x)
-            return -1;
+        for(int x:nums)
+            sum += x;
         ll t = sum - x;
-        int i=0, j=-1;
-        ll curr = 0;
-        int ans = 0;
-        // cout<<sum<<" "<<t;
+        if(t<0)
+            return -1;
         if(t==0)
             return n;
-        for(int j=0;j<n;j++)
+        unordered_map<ll, ll>m;
+        ll cnt = 0;
+        ll ans = INT_MIN;
+        for(ll i=0;i<n;i++)
         {
-            // j++;
-            curr += nums[j];
-            while(i<n and curr>t)
+            cnt += nums[i];
+            if(cnt==t)
             {
-                curr -= nums[i];
-                i++;
+                ans = max(ans, i+1);
+                // cout<<ans<<" ";
             }
-            if(curr==t)
+            else
             {
-                // cout<<"k";
-                ans = max(ans, j-i+1);
-                // cout<<i<<" "<<j<<endl;
+                ll temp = cnt-t;
+                if(m[temp]!=0)
+                    ans = max(ans, i-m[temp]+1);
             }
+            m[cnt] = i+1;
         }
-        if(ans==0)
+        if(ans==INT_MIN)//?-1:ans;
             return -1;
         return n-ans;
     }
