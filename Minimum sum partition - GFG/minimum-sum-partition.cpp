77fffc28b6601b7fff508e9e174f256dfed67140
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-13 15:29:31 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -6,59 +6,31 @@ using namespace std;
 class Solution{
 
   public:
-    // bool isSubsetSum(int n, int arr[], int sum){
-    //     // code here 
-    //     vector<vector<bool>>dp(n+1, vector<bool>(sum+1, false));
-    //     for(int i=0;i<=n;i++)
-    //     {
-    //         dp[i][0] = true;
-    //     }
-    //     for(int i=1;i<=n;i++)
-    //     {
-    //         for(int j=1;j<=sum;j++)
-    //         {
-    //             if(arr[i-1]<=j)
-    //                 dp[i][j] = dp[i-1][j-arr[i-1]] or dp[i-1][j];
-    //             else
-    //                 dp[i][j] = dp[i-1][j];
-    //         }
-    //     }
-    //     return dp[n][sum];
-    // }
-  
 	int minDifference(int arr[], int n)  { 
 	    // Your code goes here
 	    int sum = 0;
-	    for(int x=0;x<n;x++)
-	        sum+=arr[x];
-	    vector<vector<bool>>dp(n+1, vector<bool>(sum+1, 0));
-	    for(int i=0;i<=n;i++)
-	    {
-	        dp[i][0] = true;
-	    }
+	    for(int i=0;i<n;i++)
+	        sum +=arr[i];
+	   vector<vector<int>>dp(n+1, vector<int>(sum+1, 0));
+	   for(int i=0;i<=n;i++)
+	    dp[i][0] = true;
 	    for(int i=1;i<=n;i++)
 	    {
 	        for(int j=1;j<=sum;j++)
 	        {
-	            if(arr[i-1]<=j)
+	            if(j-arr[i-1]>=0)
 	                dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]];
-	            else
-	                dp[i][j] = dp[i-1][j];
-	                
+	               else
+	               dp[i][j] = dp[i-1][j];
 	        }
 	    }
-	    int diff = sum;
-	    int curr_sum = 0;
-	    for(int i=sum/2;i>=0;i--)
+	    int i;
+	    for(i=sum/2;i>=0;i--)
 	    {
-	        if(dp[n][i]==true)
-	        {
-	            diff = abs(sum-2*i);
+	        if(dp[n][i])
 	            break;
-	        }
 	    }
-	    return diff;
-	    
+	    return abs(sum - (2 * i));
 	} 
 };
 
