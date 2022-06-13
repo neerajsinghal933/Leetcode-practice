168d4a430973c17745b6afe617fda4f79767a3ea
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-13 13:47:41 +0530

    Time: 1491 ms (5.03%), Space: 218.1 MB (7.75%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(3,-1)));
+        vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
         return fun(prices, 0, 1, 2, dp);
+        
     }
     
-    int fun(vector<int>&p, int idx, bool buy, int k, vector<vector<vector<int>>>&dp)
+    int fun(vector<int>&v, int idx, bool buy, int k, vector<vector<vector<int>>>&dp)
     {
-        if(idx>=p.size() or k==0)
+        if(idx>=v.size() or k==0)
             return 0;
         if(dp[idx][buy][k]!=-1)
             return dp[idx][buy][k];
         if(buy)
         {
-            return dp[idx][buy][k] = max(-p[idx] + fun(p, idx+1, !buy, k, dp), fun(p, idx+1, buy, k, dp));
-            
+            return dp[idx][buy][k] = max(-v[idx] + fun(v, idx+1, !buy, k, dp), fun(v, idx+1, buy, k, dp));
         }
         else
         {
-            return dp[idx][buy][k] = max(p[idx] + fun(p, idx+1, !buy, k-1, dp), fun(p, idx+1, buy, k, dp));
+            return dp[idx][buy][k] = max(v[idx] + fun(v, idx+1, !buy, k-1, dp), fun(v, idx+1, buy, k, dp));
         }
     }
 };
