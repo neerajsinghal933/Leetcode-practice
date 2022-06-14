6b1c0a2d95ab220ca2a394c91b14f77ccdf29fa8
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-14 14:20:29 +0530

    Time: 44 ms (24.13%), Space: 15.4 MB (17.40%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
-    int dp[1001][1001];
     string shortestCommonSupersequence(string s1, string s2) {
         int n = s1.size();
         int m = s2.size();
-        // vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-        memset(dp, 0, sizeof(dp));
-        // return fun(s1, s2, n, m);
-        
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
                 if(s1[i-1]==s2[j-1])
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    dp[i][j] = 1+dp[i-1][j-1];
                 else
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         }
+        // cout<<dp[n][m];
         int i=n, j=m;
-        string res = "";
+        string ans= "";
         while(i>0 and j>0)
         {
             if(s1[i-1]==s2[j-1])
             {
-                res = s1[i-1] + res;
+                ans = s1[i-1] + ans;
                 i--;j--;
             }
-            else if(dp[i-1][j] > dp[i][j-1])
+            else if(dp[i-1][j]>dp[i][j-1])
             {
-                res = s1[i-1] + res;
+                ans = s1[i-1] + ans;
                 i--;
             }
             else
             {
-                res = s2[j-1] + res;
+                ans = s2[j-1] + ans;
                 j--;
             }
         }
         while(i>0)
         {
-            res = s1[i-1]+res;
+            ans = s1[i-1] + ans;
             i--;
         }
-        while(j>0){
-            res = s2[j-1]+res;
+        while(j>0)
+        {
+            ans = s2[j-1] + ans;
             j--;
         }
-        return res;
+        return ans;
     }
 };
