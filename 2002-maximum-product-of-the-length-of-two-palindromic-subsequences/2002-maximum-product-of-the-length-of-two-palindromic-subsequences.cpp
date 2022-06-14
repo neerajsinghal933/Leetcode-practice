89bd673f0e6909e4493bfc2388958be4586a2b1d
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-14 12:53:04 +0530

    Time: 1513 ms (21.06%), Space: 5.8 MB (94.32%) - LeetHub

--- 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -2,34 +2,33 @@ class Solution {
 public:
     int ans = INT_MIN;
     int maxProduct(string s) {
-        string s1 = "";
-        string s2 = "";
-        fun(s, s2, s1, 0);
+        int n = s.size();
+        string s1="", s2="";
+        fun(s, s1, s2, 0);
         return ans;
     }
     
-    void fun(string& s, string& s1, string& s2, int i)
+    void fun(string &s, string &s1, string &s2, int idx)
     {
-        if(i==s.size() and isPalin(s1) and isPalin(s2))
+        if(idx==s.size() and isPalin(s1) and isPalin(s2))
         {
             ans = max(ans, (int)s1.size() * (int)s2.size());
             return;
         }
-        if(i==s.size())
+        if(idx==s.size())
             return;
-        s1.push_back(s[i]);
-        fun(s, s1, s2, i+1);
+        s1 += s[idx];
+        fun(s, s1, s2, idx+1);
         s1.pop_back();
         
-        s2.push_back(s[i]);
-        fun(s, s1, s2, i+1);
+        s2.push_back(s[idx]);
+        fun(s, s1, s2, idx+1);
         s2.pop_back();
         
-        
-        fun(s, s1, s2, i+1);
+        fun(s, s1, s2, idx+1);
     }
     
-    bool isPalin(string& s)
+    bool isPalin(string s)
     {
         int n = s.size();
         for(int i=0;i<n/2;i++)
