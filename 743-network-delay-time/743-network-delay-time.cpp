61a0a72128e7f895baf605e4cba92df74531e350
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-15 22:38:33 +0530

    Time: 182 ms (46.76%), Space: 39.9 MB (81.52%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-         vector<vector<pair<int,int>>> vec(n+1);
-        
-        for(int i=0;i<times.size();i++){
-        
-           int j = times[i][0];
-           vec[j].push_back({times[i][1],times[i][2]}); 
+        vector<pair<int, int>>adj[n+1];
+        int sz = times.size();
+        for(int i=0;i<sz;i++)
+        {
+            adj[times[i][0]].push_back({times[i][2], times[i][1]});
         }
-        
-         vector<int> signal(n+1,INT_MAX);
-
-  queue<int> q;
-        q.push(k);
-        
-   
-        signal[k] = 0;
-        
-        while (!q.empty()) {
-            int currNode = q.front(); 
-            q.pop();
-            
-       
-            for (pair<int,int> p : vec[currNode]) {
-                
-                
-                int time = p.second;
-                int neighbor = p.first;
-                
-                int arrival = signal[currNode] + time;
-                if (signal[neighbor] > arrival) {
-                    signal[neighbor] = arrival;
-                    q.push(neighbor);
+        vector<int>dist(n+1, INT_MAX);
+        dist[k] = 0;
+        dist[0] = 0;
+        vector<bool>vis(n+1, 0);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+        pq.push({0, k});
+        while(!pq.empty())
+        {
+            int u = pq.top().second;
+            int dist_u = pq.top().first;
+            pq.pop();
+            if(vis[u])
+                continue;
+            vis[u] = true;
+            for(auto x:adj[u])
+            {
+                int v = x.second;
+                int d = x.first;
+                if(dist[v] > dist[u] + d)
+                {
+                    dist[v] = dist[u] + d;
+                    pq.push({dist[v], v});
                 }
-            
             }
         }
-        
-        int maxi = 0;
-        for(int i =1;i<=n;i++){
-            
-            if(signal[i]==INT_MAX)return -1;
-            
-            maxi = max(maxi,signal[i]);
-            
-        }
-                
-        return maxi;
-
+        int ans = *max_element(dist.begin(), dist.end());
+        return ans==INT_MAX?-1:ans;
     }
-    
 };
