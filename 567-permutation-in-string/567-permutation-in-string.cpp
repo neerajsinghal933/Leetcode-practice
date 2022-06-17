e0e6521ee695b45fce2bd0a083dce0a3a0e78731
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-18 01:43:32 +0530

    Time: 31 ms (28.49%), Space: 24.6 MB (14.61%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,17 +1,44 @@
 class Solution {
 public:
-	bool checkInclusion(string s1, string s2) {
-		vector<int> map_s1(26), map_s2(26);
-		for(char& i1 : s1) 
-			map_s1[i1 - 'a']++;
-		for(int i = 0; i < s2.size(); i++) {
-			map_s2[s2[i] - 'a']++;
-			if(i >= s1.size() - 1) {
-				if(map_s2 == map_s1)  // Time: O(26)
-					return true;
-				map_s2[s2[i - s1.size() + 1] - 'a']--;
-			}
-		}
-		return false;
-	}
+    bool checkInclusion(string p, string s) {
+        unordered_map<char, int>m;
+        if(p.size()>s.size())
+            return false;
+        for(char c:p)
+            m[c]++;
+        int n = s.size();
+        int k = p.size();
+        if(k>n)
+            return "-1";
+        string ans = "-1";
+        int ans_sz = INT_MAX;
+        int j=0, matched = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(m.find(s[i])!=m.end())
+            {
+                m[s[i]]--;
+                if(m[s[i]]>=0)
+                    matched++;
+                while(matched==k)
+                {
+                    if(i-j+1<ans_sz)
+                    {
+                        ans_sz = i-j+1;
+                        ans = s.substr(j, i-j+1);
+                    }
+                    if(m.find(s[j])!=m.end())
+                    {
+                        m[s[j]]++;
+                        if(m[s[j]]>0)
+                            matched--;
+                    }
+                    j++;
+                }
+            }
+        }
+        if(ans=="-1")
+            return false;
+        return ans.size()==k;
+    }
 };
