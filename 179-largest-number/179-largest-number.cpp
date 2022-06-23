97bd8e7bd7754324bb5aae3b5b88a4a4cc5ad80d
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-23 11:44:45 +0530

    Time: 23 ms (19.46%), Space: 11.5 MB (45.44%) - LeetHub

---
+++ 179-largest-number/179-largest-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    static bool comp(string a, string b)
+    {
+        return a+b>b+a;
+}
+    
+    string largestNumber(vector<int>& nums) {
+        vector<string>v;
+        // sort(nums.begin(), nums.end(), comp);
+        int cnt = 0;
+        for(int x:nums)
+        {
+            v.push_back(to_string(x));
+            if(x==0)
+            cnt++;
+        }
+        if(cnt==nums.size())
+            return "0";
+        sort(v.begin(), v.end(), comp);
+        string ans = "";
+        for(string s:v)
+            ans += s;
+        return ans;
+    }
+};
