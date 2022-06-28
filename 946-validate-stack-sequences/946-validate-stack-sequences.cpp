f6a99d81409946e9191b4d3de7d64e3dfc455618
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-28 14:30:06 +0530

    Time: 11 ms (75.34%), Space: 15.4 MB (62.43%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        int i = 0, j = 0;
-        for(int x :  pushed)
+        int i=0;
+        stack<int>st;
+        for(int x:pushed)
         {
-            pushed[i++] = x;
-            // check that current pushed element matches with popped sequence
-            while(i> 0 && pushed[i-1] == popped[j])
+            st.push(x);
+            while(!st.empty() and st.top()==popped[i])
             {
-                --i;
-                ++j;
+                i++;
+                st.pop();
             }
         }
-        // as pushed is a permutation of stack so at end it should be empty
-        return i == 0;
+        return i==popped.size();
     }
 };
