fc7fb66b265f0d2ae1d6e62ad82beef6b248b374
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-06-30 11:50:53 +0530

    Time: 20 ms (31.03%), Space: 10.8 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -6,18 +6,19 @@ public:
         sort(nums.begin(), nums.end());
         
         int first = nums[n/2];
-        int second;
-        if(n%2==0)
-            second = nums[(n-1)/2];
-        int cnt1=0, cnt2 =0;
+        // int second;
+        // if(n%2==0)
+        //     second = nums[(n-1)/2];
+        int cnt1=0;//, cnt2 =0;
         for(int i=0;i<n;i++)
         {
             cnt1 += abs(first-nums[i]);
-            if(n%2==0)
-                cnt2 += abs(second-nums[i]);
+            // if(n%2==0)
+            //     cnt2 += abs(second-nums[i]);
         }
-        if(n%2)
-            return cnt1;
-        return min(cnt1, cnt2);
+        // if(n%2)
+        //     return cnt1;
+        // return min(cnt1, cnt2);
+        return cnt1;
     }
 };
