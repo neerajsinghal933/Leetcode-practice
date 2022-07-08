d5c005e441f35a230f3452e64a5c46ebd601b552
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-08 10:50:24 +0530

    Time: 1568 ms (5.00%), Space: 10.4 MB (52.52%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -7,19 +7,19 @@ public:
         int n = nums.size();
         
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        return solve(nums, 1, n-1, dp);
+        return solve(nums, 1, n-2, dp);
     }
     
     int solve(vector<int>& nums, int l, int r, vector<vector<int>> &dp){
-        if(l >= r) return 0;
-        if(dp[l][r] != -1) return dp[l][r];
-        
-        int ans = 0; 
-        
-        for(int i = l; i<r; i++){
-            int temp = solve(nums, l, i, dp) + solve(nums, i+1, r, dp) + nums[l-1]*nums[i]*nums[r];
-            ans = max(ans, temp);
+        if(l>r)
+            return 0;
+        if(dp[l][r]!=-1)
+            return dp[l][r];
+        for(int i=l;i<=r;i++)
+        {
+            int ans = solve(nums, l, i-1, dp) + solve(nums, i+1, r, dp) + (nums[l-1] * nums[i] * nums[r+1]);
+            dp[l][r] = max(dp[l][r], ans);
         }
-        return dp[l][r] = ans;
+        return dp[l][r];
     } 
 };
