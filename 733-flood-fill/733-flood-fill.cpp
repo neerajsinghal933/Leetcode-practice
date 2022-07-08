269d375c9a1b074c7c05f890db9f4aae56a9075f
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-08 21:33:30 +0530

    Time: 3 ms (99.60%), Space: 14.6 MB (8.09%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+    int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         queue<pair<int, int>>q;
+        int oldColor = image[sr][sc];
+        q.push({sr, sc});
         int n = image.size();
         int m = image[0].size();
-        int oldColor = image[sr][sc];
-        image[sr][sc] = newColor;
-        q.push({sr, sc});
+        image[sr][sc] = color;
         vector<vector<int>>vis(n, vector<int>(m, 0));
-        vis[sr][sc]=1;
+        vis[sr][sc] = 1;
         while(!q.empty())
         {
             int x = q.front().first;
             int y = q.front().second;
-            vis[x][y] = 1;
             q.pop();
-            if(x-1 >= 0 and image[x-1][y]==oldColor and vis[x-1][y]==0)
+            vis[x][y] = 1;
+            for(int i=0;i<4;i++)
             {
-                image[x-1][y] = newColor;
-                q.push({x-1, y});
-            }
-            if(x+1 < n and image[x+1][y]==oldColor and vis[x+1][y]==0)
-            {
-                image[x+1][y] = newColor;
-                q.push({x+1, y});
-            }
-            if(y-1 >= 0 and image[x][y-1]==oldColor and vis[x][y-1]==0)
-            {
-                image[x][y-1] = newColor;
-                q.push({x, y-1});
-            }
-            if(y+1 < m and image[x][y+1]==oldColor and vis[x][y+1]==0)
-            {
-                image[x][y+1] = newColor;
-                q.push({x, y+1});
+                int curr_x = x + dir[i][0];
+                int curr_y = y + dir[i][1];
+                if(curr_x>=0 and curr_y>=0 and curr_x<n and curr_y<m and image[curr_x][curr_y] == oldColor and vis[curr_x][curr_y]==0)
+                {
+                    image[curr_x][curr_y] = color;
+                    q.push({curr_x, curr_y});
+                }
             }
         }
         return image;
