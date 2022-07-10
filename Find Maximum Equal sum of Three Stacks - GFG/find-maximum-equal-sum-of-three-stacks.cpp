518bedcbd514e276b4b2468245c383f2265101bb
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-10 23:13:10 +0530

    Added solution - LeetHub

--- Find Maximum Equal sum of Three Stacks - GFG/find-maximum-equal-sum-of-three-stacks.cpp
+++ Find Maximum Equal sum of Three Stacks - GFG/find-maximum-equal-sum-of-three-stacks.cpp
@@ -8,50 +8,35 @@ using namespace std;
  // } Driver Code Ends
 //User function Template for C++
 
-
 class Solution{
 public:
-    int maxEqualSum(int N1,int N2,int N3,vector<int> &S1,vector<int> &S2,vector<int> &S3){
-        int sum1 = accumulate(S1.begin(), S1.end(), 0);
-        int sum2 = accumulate(S2.begin(), S2.end(), 0);
-        int sum3 = accumulate(S3.begin(), S3.end(), 0);
-        // cout<<sum1<<" "<<sum2<<" "<<sum3<<endl;
-        // int sum1 = 0, sum2 = 0, sum3 = 0;
-        // for(int i = 0; i < N1; i++)
-        // {cout<<S1[i]<<" ";
-        //     sum1 += S1[i];
-        // }cout<<endl;
-        // for(int i = 0; i < N2; i++)
-        // {cout<<S2[i]<<" ";
-        //     sum2 += S2[i];
-        // }cout<<endl;
-        // for(int i = 0; i < N3; i++)
-        // {
-        //     cout<<S3[i]<<" ";
-        //     sum3 += S3[i];
-        // }cout<<endl;
-        // cout<<sum1<<" "<<sum2<<" "<<sum3<<endl;
-        int cnt = 0;
-        int i = 0, j = 0, k = 0;
-        while(sum1 != sum2 or sum2 != sum3)
-        {
-            if(sum1 >= sum2 && sum1 >= sum3)
-            {
-                sum1 -= S1[i++];
-                cnt++;
-            }
-            else if(sum2 >= sum1 && sum2 >= sum3)
-            {
-                sum2 -= S2[j++];
-                cnt++;
-            }
-            else if(sum3 >= sum1 && sum3 >= sum2)
-            {
-                sum3 -= S3[k++];
-                cnt++;
-            }
-        }
-        return sum1;
+    int maxEqualSum(int n1,int n2,int n3,vector<int> &stack1,vector<int> &stack2,vector<int> &stack3){
+        int sum1 = 0, sum2 = 0, sum3 = 0;
+ 
+    for (int i = 0; i < n1; i++)
+        sum1 += stack1[i];
+ 
+    for (int i = 0; i < n2; i++)
+        sum2 += stack2[i];
+ 
+    for (int i = 0; i < n3; i++)
+        sum3 += stack3[i];
+ 
+    int top1 = 0, top2 = 0, top3 = 0;
+    while (1) {
+        if (top1 == n1 || top2 == n2 || top3 == n3)
+            return 0;
+ 
+        if (sum1 == sum2 && sum2 == sum3)
+            return sum1;
+ 
+        if (sum1 >= sum2 && sum1 >= sum3)
+            sum1 -= stack1[top1++];
+        else if (sum2 >= sum1 && sum2 >= sum3)
+            sum2 -= stack2[top2++];
+        else if (sum3 >= sum2 && sum3 >= sum1)
+            sum3 -= stack3[top3++];
+    }
     }
 };
 
