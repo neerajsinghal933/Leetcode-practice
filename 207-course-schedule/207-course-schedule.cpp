6316af06f7dae7edd9ab2639842e3f9a92a32aec
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-10 11:13:02 +0530

    Time: 24 ms (83.86%), Space: 13.4 MB (81.53%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -3,17 +3,15 @@ public:
     bool canFinish(int n, vector<vector<int>>& p) {
         vector<int>in(n, 0);
         int sz = p.size();
-        vector<vector<int>>vis(n, vector<int>());
+        vector<vector<int>>adj(n, vector<int>());
         queue<int>q;
-        
         for(int i=0;i<sz;i++)
         {
             int a = p[i][0];
             int b = p[i][1];
-            vis[b].push_back(a);
+            adj[b].push_back(a);
             in[a]++;
         }
-        
         for(int i=0;i<n;i++)
         {
             if(in[i]==0)
@@ -22,18 +20,17 @@ public:
         int c = 0;
         while(!q.empty())
         {
-            int temp = q.front();
+            int t = q.front();
             q.pop();
             c++;
-            for(int x:vis[temp])
+            for(int x:adj[t])
             {
                 in[x]--;
                 if(in[x]==0)
                     q.push(x);
             }
         }
-        if(c==n)
-            return true;
-        return false;
+        return c==n;
     }
+    
 };
