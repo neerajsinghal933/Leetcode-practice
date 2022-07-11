7f2db241ea492c28468138cea26326ce0b52a4cb
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-11 17:49:35 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -9,26 +9,43 @@ using namespace std;
 
 class Solution{
 public:
-    int equalPartition(int n, int a[])
+    int equalPartition(int n, int arr[])
     {
         // code here
-        int sum = 0;
-        for(int i=0;i<n;i++)
-            sum += a[i];
-        if(sum & 1)
+        int sum = accumulate(arr, arr+n, 0);
+        if(sum&1)
             return 0;
-        return fun(n-1, a, sum/2, 0);
+        sum /= 2;
+        return fun(n, arr, sum);
     }
     
-    bool fun(int n, int a[], int k, int curr)
+    bool fun(int n, int arr[], int s)
     {
-        if(n==0 and k!=curr)
-            return 0;
-        if(k==curr)
-            return 1;
-        if(curr + a[n-1] <= k)
-            return fun(n-1, a, k, curr +a[n-1]) or fun(n-1, a, k, curr);
-        return fun(n-1, a, k, curr);
+        vector<vector<int>>dp(n+1, vector<int>(s+1, 0));
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=s;j++)
+            {
+                if(arr[i-1]<=j)
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][s];
+        // if(n==0 or s==0)
+        // {
+        //     if(s==0)
+        //         return true;
+        //     return false;
+        // }
+        // bool p = false;
+        // if(arr[n-1]<=s)
+        //     p =fun(n-1, arr, s-arr[n-1]);
+        // bool q = fun(n-1, arr, s);
+        // return p or q;
     }
 };
 
