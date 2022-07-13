0db6c7c882ef31c1df71fb4f761324526572ece0
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-13 16:33:00 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -9,28 +9,59 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	static bool comp(vector<int>&a, vector<int>&b)
+	{
+	    return a[2]<b[2];
+	}
+	
+	int find_set(int x, vector<int>&par)
+    {
+        if(par[x]==-1)
+            return x;
+        return par[x] = find_set(par[x], par);
+    }
+    
+    int union_set(int x, int y, vector<int>&par, vector<int>&rank)
+    {
+        if(rank[x]<rank[y])
+        {
+            par[x] = y;
+            rank[y] += rank[x];
+        }
+        else{
+            par[y] = x;
+            rank[x] += rank[y];
+        }
+    }
+	
     int spanningTree(int v, vector<vector<int>> adj[])
     {
-        vector<bool>vis(v, false);
-        vector<int>dist(v, INT_MAX);
-        int cnt = 0;
-        int src = 0;
-        while(1)
-        {
-            vis[src] = 1;
-            for(auto x:adj[src])
-            {
-                if(vis[x[0]])
-                    continue;
-                dist[x[0]] = min(dist[x[0]], x[1]);
-            }
-            src = min_element(dist.begin(), dist.end()) - dist.begin();
-            if(dist[src]==INT_MAX)
-                return cnt;
-            cnt += dist[src];
-            dist[src] = INT_MAX;
-        }
         // code here
+        vector<vector<int>>graph;
+        for(int i=0;i<v;i++)
+        {
+            for(auto x:adj[i])
+            {
+                graph.push_back({i, x[0], x[1]});
+            }
+        }
+        sort(graph.begin(), graph.end(), comp);
+        vector<int>par(v, -1), rank(v, 1);
+        int ans = 0;
+        for(int i=0;i<graph.size();i++)
+        {
+            int u = graph[i][0];
+            int v = graph[i][1];
+            int d = graph[i][2];
+            int s1 = find_set(u, par);
+            int s2 = find_set(v, par);
+            if(s1!=s2)
+            {
+                ans += d;
+                union_set(s1, s2, par, rank);
+            }
+        }
+        return ans;
     }
 };
 
