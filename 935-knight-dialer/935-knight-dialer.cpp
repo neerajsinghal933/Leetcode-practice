bc8075c85e33a6b9a175299516b96ab904e0f7d4
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-16 14:09:18 +0530

    Time: 195 ms (65.00%), Space: 6.6 MB (77.16%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.cpp
+++ 935-knight-dialer/935-knight-dialer.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    typedef long long ll;
     int mod = 1e9+7;
     int dp[4][3][5001];
+    
     int dirX[8] = {1, 1, -1, -1, 2, 2, -2, -2};
     int dirY[8] = {2, -2, 2, -2, 1, -1, 1, -1};
+    
     int knightDialer(int n) {
         memset(dp, -1, sizeof(dp));
-        ll ans = 0;
-        ans += fun(3, 1, n-1);
+        long long ans = fun(3, 1, n-1);
         for(int i=1;i<=9;i++)
         {
             ans += fun((i-1)/3, (i-1)%3, n-1);
             ans %= mod;
         }
         return ans;
+    }
+    
+    bool isSafe(int x, int y)
+    {
+        if(x<0 or x>3)
+            return false;
+        if(y<0 or y>2)
+            return false;
+        if(x==3 and y!=1)
+            return false;
+        return true;
     }
     
     int fun(int x, int y, int n)
@@ -25,23 +36,13 @@ public:
             return 1;
         if(dp[x][y][n]!=-1)
             return dp[x][y][n];
-        ll ans = 0;
+        long long ans = 0;
         for(int i=0;i<8;i++)
         {
-            ans += fun(x + dirX[i], y + dirY[i], n-1);
+            ans += fun(x+dirX[i], y+dirY[i], n-1);
             ans %= mod;
         }
         return dp[x][y][n] = ans;
-    }
-    
-    bool isSafe(int x, int y)
-    {
-        if(x<0 or x>3)
-            return 0;
-        if(y<0 or y>2)
-            return 0;
-        if(x==3 and y!=1)
-            return 0;
-        return 1;
+        
     }
 };
