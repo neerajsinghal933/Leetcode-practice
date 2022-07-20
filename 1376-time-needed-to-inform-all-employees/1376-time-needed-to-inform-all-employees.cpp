6666acee19440de3518719d5435f63fa39e100c3
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-20 14:45:53 +0530

    Time: 459 ms (59.22%), Space: 117.5 MB (74.39%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -11,21 +11,40 @@ public:
             adj[x].push_back(i);
         }
         int time = 0;
-        dfs(adj, informTime, time, headID);
-        return res;
+        
+        queue<pair<int, int>>q;
+        q.push({headID, 0});
+        while(!q.empty())
+        {
+            auto p = q.front();
+            q.pop();
+            if(adj[p.first].size()==0)
+            {
+                time = max(time, p.second);
+                continue;
+            }
+            for(int i=0;i<adj[p.first].size();i++)
+            {
+                q.push({adj[p.first][i], p.second + informTime[p.first]});
+            }
+        }
+        return time;
+        
+        // dfs(adj, informTime, time, headID);
+        // return res;
     }
     
-    void dfs(vector<int>adj[], vector<int>&informTime, int time, int node)
-    {
-        if(adj[node].size()==0)
-        {
-            res = max(res, time);
-            return;
-        }
-        time += informTime[node];
-        for(int x:adj[node])
-        {
-            dfs(adj, informTime, time, x);
-        }
-    }
+    // void dfs(vector<int>adj[], vector<int>&informTime, int time, int node)
+    // {
+    //     if(adj[node].size()==0)
+    //     {
+    //         res = max(res, time);
+    //         return;
+    //     }
+    //     time += informTime[node];
+    //     for(int x:adj[node])
+    //     {
+    //         dfs(adj, informTime, time, x);
+    //     }
+    // }
 };
