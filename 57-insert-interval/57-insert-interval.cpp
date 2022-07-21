41cfea7d329ef18ff8a9ed91c06376173420a7bd
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-21 23:12:16 +0530

    Time: 17 ms (83.14%), Space: 17 MB (74.50%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,14 +1,66 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& v, vector<int>& newInterval) {
-        v.push_back(newInterval);
+        // v.push_back(newInterval);
         int n = v.size();
-        sort(v.begin(), v.end());
-        vector<vector<int>>res;
-        int a = v[0][0];
-        int b = v[0][1];
-        for(int i=1;i<n;i++)
+        // sort(v.begin(), v.end());
+        
+        int pos = -1;
+        int val = newInterval[0];
+        int l = 0, h = n-1;
+        while(l<=h)
         {
+            int mid = (l+h)/2;
+            if(v[mid][0]<=val)
+            {
+                pos = mid;
+                l = mid+1;
+            }
+            else
+                h = mid-1;
+        }
+        // cout<<pos+1;
+        // v.insert(pos+1, newInterval);
+        
+        vector<vector<int>>res;
+        if(n==0)
+        {
+            res.push_back(newInterval);
+            return res;
+        }
+        int a, b;
+        if(pos+1==0)
+        {
+            a = newInterval[0];
+            b = newInterval[1];
+            if(v[0][0]<=b)
+                b = max(b, v[0][1]);
+            else
+            {
+                res.push_back({a, b});
+                a = v[0][0];
+                b = v[0][1];
+            } 
+        }
+        else{
+             a = v[0][0];
+             b = v[0][1];
+        }
+        int i;
+        for(i=1;i<n;i++)
+        {
+            if(i==pos+1)
+            {
+               if(newInterval[0]<=b)
+                    b = max(b, newInterval[1]);
+                else
+                {
+                    res.push_back({a, b});
+                    a = newInterval[0];
+                    b = newInterval[1];
+                } 
+            }
+            
             if(v[i][0]<=b)
                 b = max(b, v[i][1]);
             else
@@ -17,7 +69,21 @@ public:
                 a = v[i][0];
                 b = v[i][1];
             }
+            
         }
+        
+        if(i==pos+1)
+        {
+           if(newInterval[0]<=b)
+                b = max(b, newInterval[1]);
+            else
+            {
+                res.push_back({a, b});
+                a = newInterval[0];
+                b = newInterval[1];
+            } 
+        }
+            
         res.push_back({a, b});
         return res;
     }
