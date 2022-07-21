ac780bc2796d4293c413d5d8a17969bd5bbd7969
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-21 15:34:59 +0530

    Time: 10 ms (72.99%), Space: 9 MB (76.27%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    map<vector<int>, int>m;
-    void fun(vector<int>&nums, int idx, vector<vector<int>>&ans)
-    {
-        if(idx==nums.size())
-        {
-            if(!m[nums])
-            {
-                ans.push_back(nums);
-                m[nums]++;
-            }
-            return;
-        }
+    vector<vector<int>>ans;
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        unordered_map<int, int>m;
+        for(int x:nums)
+            m[x]++;
+        vector<int>temp;
+        fun(m, nums.size(), temp);
+        return ans;
         
-        for(int i=idx;i<nums.size();i++)
-        {
-            swap(nums[i], nums[idx]);
-            fun(nums, idx+1, ans);
-            swap(nums[i], nums[idx]);
-        }
     }
     
-    vector<vector<int>> permuteUnique(vector<int>& ar) {
-      
-        vector<vector<int>>ans;
-        fun(ar, 0, ans);
-        return ans;
+    void fun(unordered_map<int, int>&m, int n, vector<int>&temp)
+    {
+        if(n==0)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        for(auto &it:m)
+        {
+            if(it.second<=0)
+                continue;
+            it.second--;
+            temp.push_back(it.first);
+            fun(m, n-1, temp);
+            temp.pop_back();
+            it.second++;
+        }
     }
 };
