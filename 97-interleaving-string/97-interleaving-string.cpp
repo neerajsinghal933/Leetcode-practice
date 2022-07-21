8bbd591734b218d3cd761f4e1b99527193d2ca55
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-21 23:41:11 +0530

    Time: 3 ms (82.59%), Space: 6.3 MB (86.77%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int dp[1001][1001];
+    int dp[101][101];
     
-    bool fun(string &a, string &b, string &c, int n, int m, int sz)
-    {
-        if(sz==0)
-        return true;
-        if(dp[n][m] != -1)
-            return dp[n][m];
-        bool p = false, q = false;
-        if(n-1>=0 and a[n-1]==c[sz-1])
-            p = fun(a, b, c, n-1, m, sz-1);
-        if(m-1>=0 and b[m-1]==c[sz-1])
-            q = fun(a, b, c, n, m-1, sz-1);
-        return dp[n][m] = p or q;
+    bool isInterleave(string s1, string s2, string s3) {
+        int a = s1.size();
+        int b = s2.size();
+        int c = s3.size();
+        if(a+b!=c)
+            return false;
+        memset(dp, -1, sizeof(dp));
+        return fun(s1, s2, s3, a, b, c);
     }
     
-    bool isInterleave(string A, string B, string C) {
-         int n = A.size();
-        int m = B.size();
-        int sz = C.size();
-        
-        if(n+m!=sz)
-        return 0;
-        dp[n][m];
-        memset(dp, -1, sizeof(dp));
-        return fun(A, B, C, n, m, sz);
+    bool fun(string& s1, string& s2, string& s3, int a, int b, int c)
+    {
+        if(c==0)
+            return true;
+        if(dp[a][b]!=-1)
+            return dp[a][b];
+        bool p = false, q = false;
+        if(a-1>=0 and s1[a-1]==s3[c-1])
+            p = fun(s1, s2, s3, a-1, b, c-1);
+        if(b-1>=0 and s2[b-1] == s3[c-1])
+            q = fun(s1, s2, s3, a, b-1, c-1);
+        return dp[a][b] = p or q;
     }
 };
