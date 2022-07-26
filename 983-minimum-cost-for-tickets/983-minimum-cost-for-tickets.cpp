b2b77db66e928e36a57bd9001055c1e510c3d0a1
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-26 16:23:55 +0530

    Time: 6 ms (61.09%), Space: 9.5 MB (84.79%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int dp[366];
     int mincostTickets(vector<int>& days, vector<int>& costs) {
+        // vector<int>dp
         memset(dp, -1, sizeof(dp));
         int n = days.size();
-        int k = 0;
-        return fun(days, costs, n, k);
+        return fun(days, costs, n, 0);
     }
     
-    int fun(vector<int>& days, vector<int>&costs, int n, int k)
+    int fun(vector<int>&days, vector<int>&costs, int n, int k)
     {
         if(k>=n)
             return 0;
@@ -19,11 +19,10 @@ public:
         while(x<n and days[x]<days[k]+7)
             x++;
         int b = costs[1] + fun(days, costs, n, x);
-        int y = k;
-        while(y<n and days[y]<days[k]+30)
-            y++;
-        int c = costs[2] + fun(days, costs, n, y);
+        x = k;
+        while(x<n and days[x]<days[k]+30)
+            x++;
+        int c = costs[2] + fun(days, costs, n, x);
         return dp[k] = min({a, b, c});
-        
     }
 };
