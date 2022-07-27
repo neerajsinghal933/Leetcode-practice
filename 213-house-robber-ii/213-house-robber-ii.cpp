0d46601c6ffe11b287145bf2c7c1250413076599
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-07-27 16:46:16 +0530

    Time: 3 ms (58.27%), Space: 7.9 MB (61.89%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -19,6 +19,6 @@ public:
         dp[1] = max(v[0], v[1]);
         for(int i=2;i<n;i++)
             dp[i] = max(dp[i-1], v[i] + dp[i-2]);
-        return max(dp[n-1], dp[n-2]);
+        return dp[n-1];
     }
 };
