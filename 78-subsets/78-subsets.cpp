c8468991bfecdd73d93fd31f1205897aff4528a8
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-08-01 15:47:04 +0530

    Time: 13 ms (7.76%), Space: 14.9 MB (35.13%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -4,21 +4,29 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int>ans;
         int n = nums.size();
-        fun(nums, 0, ans);
+        int mask = 0;
+        fun(nums, 0, ans, mask);
         return res;
     }
     
-    void fun(vector<int>&nums, int x, vector<int>ans)
+    void fun(vector<int>&nums, int x, vector<int>ans, int &mask)
     {
         if(x==nums.size())
         {
+            for(int i=0;i<nums.size();i++)
+            {
+                if(mask & (1<<i))
+                    ans.push_back(nums[i]);
+            }
             res.push_back(ans);
             return;
         }
-        ans.push_back(nums[x]);
-        fun(nums, x+1, ans);
-        ans.pop_back();
-        fun(nums, x+1, ans);
+        // ans.push_back(nums[x]);
+        mask = mask | (1<<x);
+        fun(nums, x+1, ans, mask);
+        // ans.pop_back();
+        mask = mask & ~(1<<x);
+        fun(nums, x+1, ans, mask);
         
     }
 };
