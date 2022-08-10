46dfce4bc8c70cbc41b74f04a17d483fbf95ed9a
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-08-10 12:28:21 +0530

    Time: 8 ms (34.15%), Space: 11.9 MB (88.40%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -12,22 +12,22 @@
 class Solution {
 public:
     
-    unordered_map<int, int>m;
+    // unordered_map<int, int>m;
     vector<int> rightSideView(TreeNode* root) {
         vector<int>res;
-        fun(root, 0, 0, res);
+        fun(root, 0, res);
         return res;
     }
     
-    void fun(TreeNode* root, int curr, int hei, vector<int>&res)
+    void fun(TreeNode* root, int curr, vector<int>&res)
     {
         if(!root)
             return;
-        if(m[curr]==0 and curr==hei){
-            m[curr] = 1;
+        if(res.size()==curr){
+            // m[curr] = 1;
             res.push_back(root->val);
         }
-        fun(root->right, curr+1, hei+1, res);
-        fun(root->left, curr+1, hei+1, res);
+        fun(root->right, curr+1, res);
+        fun(root->left, curr+1, res);
     }
 };
