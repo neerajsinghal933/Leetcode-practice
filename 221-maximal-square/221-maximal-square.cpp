b542930e83fce52a3ec017585b5b38eaa930486d
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-08-11 19:34:57 +0530

    Time: 123 ms (34.33%), Space: 18.2 MB (11.31%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+    int maximalSquare(vector<vector<char>>& v) {
+        int n = v.size();
+        int m = v[0].size();
+        
         vector<vector<int>>dp(n, vector<int>(m, 0));
-        int ans = 0;
+        int cnt = 0;
+        
         for(int i=0;i<n;i++)
         {
-            if(matrix[i][0] == '1')
+            for(int j=0;j<m;j++)
             {
-                dp[i][0] = 1;
-                ans = 1;
-            }
-        }
-        for(int i=0;i<m;i++)
-        {
-            if(matrix[0][i] == '1')
-            {
-                dp[0][i] = 1;
-                ans = 1;
-            }
-        }
-        // int ans = 0;
-        for(int i=1;i<n;i++)
-        {
-            for(int j=1;j<m;j++)
-            {
-                if(matrix[i][j]=='1')
+                if(i==0 or j==0)
                 {
-                    dp[i][j] = 1 + min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
+                    if(v[i][j]=='1')
+                        dp[i][j] = 1;
+                    // continue;
                 }
-                ans = max(ans, dp[i][j]);
+                else
+                {
+                    if(v[i][j]=='1')
+                    {
+                        dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    }
+                }
+                cnt = max(cnt, dp[i][j]);
             }
         }
-        return ans*ans;
+        return cnt*cnt;
     }
 };
