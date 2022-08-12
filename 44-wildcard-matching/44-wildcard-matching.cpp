55c6b8fc251035e5836173d2381c7094acb01a89
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-08-12 20:10:32 +0530

    Time: 55 ms (80.03%), Space: 27.9 MB (13.37%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -3,31 +3,32 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        
         vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-        return fun(s, p, n, m, dp);
+        return  fun(s, p, n, m, dp);
     }
     
-    int fun(string &s, string &p, int i, int j, vector<vector<int>>&dp)
+    int fun(string &s, string& p, int i, int j, vector<vector<int>>&dp)
     {
         if(i==0 and j==0)
             return 1;
         if(dp[i][j]!=-1)
             return dp[i][j];
         if(j==0)
+        {
             return dp[i][j] = 0;
+        }
         if(i==0)
         {
             if(p[j-1]!='*')
+            {
                 return dp[i][j] = 0;
+            }
             return dp[i][j] = fun(s, p, i, j-1, dp);
         }
         if(s[i-1]==p[j-1] or p[j-1]=='?')
             return dp[i][j] = fun(s, p, i-1, j-1, dp);
         if(p[j-1]=='*')
-        {
-            return dp[i][j] = fun(s, p, i, j-1, dp) or fun(s, p, i-1, j, dp);
-        }
+            return dp[i][j] = fun(s, p, i-1, j, dp) or fun(s, p, i, j-1, dp);
         return dp[i][j] = 0;
     }
 };
