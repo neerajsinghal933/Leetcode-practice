bd25ac8790892e0e61386ef372b0dc243ddb4c8b
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-08-13 16:02:21 +0530

    Time: 393 ms (31.35%), Space: 13 MB (18.37%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> dp;
+    int helper(int n){
+    if(n < 0){
+        return INT_MAX;
+    }
+    if(n == 0){
+        return 0;
+    }
+    if(dp[n] != -1){
+        return dp[n];
+    }
+    int mi = INT_MAX;
+    for(int i = 1; i*i <=n; i++){
+        mi = min(helper(n - i*i),mi);
+    }
+    dp[n] = mi + 1;
+    return mi + 1;
+}
+
+    int numSquares(int n) {
+        for(int i = 0; i <= n; i++){
+            dp.push_back(INT_MAX);
+        }
+        // return helper(n);
+        
+        dp[0] = 0;
+        for(int i=1;i<=n;i++)
+        {
+            
+            for(int j=1;j*j<=i;j++)
+            {
+                if(i-j*j>=0)
+                dp[i] = min(dp[i], dp[i-j*j]+1);    
+            }
+        }
+        return dp[n];
+    }
+};
