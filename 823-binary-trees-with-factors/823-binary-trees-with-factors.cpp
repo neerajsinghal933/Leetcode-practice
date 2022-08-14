8874cf93fda0462f6e3295190a66fe698db9d487
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-08-14 12:22:06 +0530

    Time: 96 ms (66.77%), Space: 9.2 MB (67.38%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-     int mod = pow(10,9) + 7;
     int numFactoredBinaryTrees(vector<int>& arr) {
+        int mod = 1e9+7;
+        int n = arr.size();
         sort(arr.begin(), arr.end());
-        unordered_map <int, long> rootWithCount;
-        rootWithCount[arr[0]] = 1;
-        for(int i = 1; i < arr.size(); i++){
-            long count = 1;
-            for(auto j : rootWithCount){
-                int rootEle = j.first;
-                if(arr[i] % rootEle == 0 && rootWithCount.find(arr[i] / rootEle) != rootWithCount.end()){
-                    count += rootWithCount[rootEle] * rootWithCount[arr[i] / rootEle];
-                    
+        unordered_map<int, long>m;
+        m[arr[0]] = 1;
+        for(int i=1;i<n;i++)
+        {
+            long cnt = 1;
+            for(auto x:m)
+            {
+                int ele = x.first;
+                if(arr[i]%ele==0 and m.find(arr[i]/ele)!=m.end())
+                {
+                    cnt += m[arr[i]/ele] * m[ele];
                 }
             }
-            rootWithCount[arr[i]] = count;
+            m[arr[i]] = cnt;
         }
-        int noOfTrees = 0;
-        for(auto i : rootWithCount){
-            noOfTrees = (noOfTrees + i.second) % mod;
-        }
-        return noOfTrees;
+        int ans = 0;
+        for(auto x:m)
+            ans =(ans + x.second)%mod;
+        return ans;
     }
 };
