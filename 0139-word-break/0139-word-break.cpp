c56192023878081c902c992aa7415ef849fa463c
neerajsinghal933 <55788274+neerajsinghal933@users.noreply.github.com>
2022-10-30 21:37:48 +0530

    Time: 7 ms (86.62%), Space: 8 MB (82.39%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<int>dp(n+1, 0);
+        dp[n] = 1;
+        for(int i=n-1;i>=0;i--)
+        {
+            string t = "";
+            for(int j=i;j<n;j++)
+            {
+                t += s[j];
+                if(find(wordDict.begin(), wordDict.end(), t)!=wordDict.end() and dp[j+1])
+                {
+                    dp[i] = 1;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+};
